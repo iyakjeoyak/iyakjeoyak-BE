52bab68719353af6aa1bd65430196881d493ea4e
(Jun) <(ddoly0106@gmail.com)>
2024-04-16 19:54:20 +0900

    fix : 후기 좋아요 API 수정(@Authenticaion 변경)

--- src/main/java/com/example/demo/web/controller/HeartReviewController.java
+++ src/main/java/com/example/demo/web/controller/HeartReviewController.java
@@ -1,9 +1,7 @@
 package com.example.demo.web.controller;
 
-import com.example.demo.domain.entity.HeartReview;
 import com.example.demo.service.HeartReviewService;
-import com.example.demo.web.result.PageResult;
-import com.example.demo.web.result.ReviewResult;
+import com.example.demo.web.payload.HeartReviewPayload;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,13 +9,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +19,8 @@ import java.util.List;
 @RequestMapping("/heart/review")
 public class HeartReviewController {
     private final HeartReviewService heartReviewService;
+
+    //ToDO : 시큐리티 Authentication 적용 후 User 식별 변경 해야함
 
     @GetMapping("/count/{reviewId}")
     @Operation(summary = "영양제 리뷰 좋아요 수 조회", description = "특정 리뷰의 좋아요 수")
@@ -40,7 +36,7 @@ public class HeartReviewController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Boolean.class))),
             @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
-    public ResponseEntity<Boolean> checkHeartReview(@PathVariable("reviewId") Long reviewId, @AuthenticationPrincipal Long userId) {
+    public ResponseEntity<Boolean> checkHeartReview(@PathVariable("reviewId") Long reviewId, @RequestParam("userId") Long userId) {
         return new ResponseEntity<>(heartReviewService.checkReviewHeart(userId, reviewId), HttpStatus.OK);
     }
 
@@ -49,15 +45,15 @@ public class HeartReviewController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "성공", content = @Content(schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
-    public ResponseEntity<Long> saveHeartReview(@RequestBody Long reviewId, @AuthenticationPrincipal Long userId) throws IllegalAccessException {
-        return new ResponseEntity<>(heartReviewService.save(userId, reviewId), HttpStatus.OK);
+    public ResponseEntity<Long> saveHeartReview(@RequestBody HeartReviewPayload payload) throws IllegalAccessException {
+        return new ResponseEntity<>(heartReviewService.save(payload.getUserId(), payload.getReviewId()), HttpStatus.OK);
     }
     @DeleteMapping("/{reviewId}")
     @Operation(summary = "영양제 리뷰 좋아요 삭제", description = "쿼리파라미터로 review PK")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
-    public ResponseEntity<Long> deleteHeartReview(@PathVariable("reviewId") Long reviewId, @AuthenticationPrincipal Long userId) throws IllegalAccessException {
+    public ResponseEntity<Long> deleteHeartReview(@PathVariable("reviewId") Long reviewId, @RequestParam("userId") Long userId) throws IllegalAccessException {
         return new ResponseEntity<>(heartReviewService.delete(userId, reviewId), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/demo/web/payload/HeartReviewPayload.java
@@ -0,0 +1,9 @@
+package com.example.demo.web.payload;
+
+import lombok.Data;
+
+@Data
+public class HeartReviewPayload {
+    private Long reviewId;
+    private Long userId;
+}
