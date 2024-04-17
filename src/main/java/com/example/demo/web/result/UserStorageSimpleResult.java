5708b9a96efdbfdcc21370b90926cdb5c4959e66
(Jun) <(ddoly0106@gmail.com)>
2024-04-17 19:49:27 +0900

    fix : 스웨거 스키마 수정

--- src/main/java/com/example/demo/web/result/UserStorageSimpleResult.java
+++ src/main/java/com/example/demo/web/result/UserStorageSimpleResult.java
@@ -1,5 +1,6 @@
 package com.example.demo.web.result;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +11,14 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 public class UserStorageSimpleResult {
+
+    @Schema(description = "보관 영양제 PK")
     private Long id;
 
+    @Schema(description = "유저 설정 이름")
     private String medicineName;
 
+    @Schema(description = "유통기한")
     private LocalDateTime expirationDate;
 
 }
