2a1a6faf0d1cf288444561ee8ba828002ed3ad18
(Jun) <(ddoly0106@gmail.com)>
2024-04-18 21:00:24 +0900

    fix : 충돌 수정

--- src/main/java/com/example/demo/module/review/controller/ReviewController.java
+++ src/main/java/com/example/demo/module/review/controller/ReviewController.java
@@ -66,7 +66,7 @@ public class ReviewController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = Long.class))),
             @ApiResponse(responseCode = "500", description = "에러", content = @Content(schema = @Schema(implementation = String.class)))})
-    public ResponseEntity<Long> deleteReview(@PathVariable("reviewId") Long reviewId, @RequestParam("userId") Long userId) {
+    public ResponseEntity<Long> deleteReview(@PathVariable("reviewId") Long reviewId, @RequestParam("userId")Long userId) {
         return new ResponseEntity<>(reviewService.deleteByReviewId(userId,reviewId), HttpStatus.OK);
     }
 
