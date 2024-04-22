3d4236d87de35dceb630ad76a999dee20887775f
SeungjunNA <nsjjsng1@gmail.com>
2024-04-22 23:42:11 +0900

    fix: 영양제 반환값 정보 수정

--- src/main/java/com/example/demo/module/medicine/dto/result/MedicineResult.java
+++ src/main/java/com/example/demo/module/medicine/dto/result/MedicineResult.java
@@ -1,9 +1,14 @@
 package com.example.demo.module.medicine.dto.result;
 
+import com.example.demo.module.category.dto.result.CategoryResult;
+import com.example.demo.module.hashtag.dto.result.HashtagResult;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,12 +24,9 @@ public class MedicineResult {
     @Schema(description = "제품 명")
     private String PRDLST_NM;
 
-    @Schema(description = "좋아요 수")
-    private Integer heartCount;
+    @Schema(description = "카테고리 정보")
+    private List<CategoryResult> categories = new ArrayList<>();
 
-    @Schema(description = "평점")
-    private Long star;
-
-    @Schema(description = "리뷰 수")
-    private Integer reviewCount;
+    @Schema(description = "해쉬태그 정보")
+    private List<HashtagResult> hashtags = new ArrayList<>();
 }
