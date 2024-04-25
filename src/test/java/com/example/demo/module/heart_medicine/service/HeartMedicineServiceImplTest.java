a168c8f119dab7f0a0875ba4905ced3ac0996a50
SeungjunNA <nsjjsng1@gmail.com>
2024-04-25 19:31:13 +0900

    test: 영양제 좋아요/취소 예외처리 테스트

--- src/test/java/com/example/demo/module/heart_medicine/service/HeartMedicineServiceImplTest.java
+++ src/test/java/com/example/demo/module/heart_medicine/service/HeartMedicineServiceImplTest.java
@@ -10,6 +10,7 @@ import com.example.demo.module.user.entity.User;
 import com.example.demo.module.user.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -23,6 +24,7 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,7 +46,8 @@ class HeartMedicineServiceImplTest {
         userId = 1L;
     }
     @Test
-    void like(){
+    @DisplayName("좋요아 성공")
+    void like_success(){
         Medicine medicine = Medicine.builder().id(medicineId).heartCount(0).build();
         User user = User.builder().userId(userId).build();
         HeartMedicine heartMedicine = HeartMedicine.builder().medicine(medicine).user(user).id(1L).build();
@@ -59,7 +62,33 @@ class HeartMedicineServiceImplTest {
         assertEquals(1, medicine.getHeartCount());
     }
     @Test
-    void cancel(){
+    @DisplayName("좋아요 실패(영양제 없음)")
+    void like_fail1(){
+        when(medicineRepository.findById(anyLong())).thenReturn(Optional.empty());
+        when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(false);
+
+        assertThrows(NoSuchElementException.class, () -> heartMedicineService.like(userId, medicineId));
+    }
+    @Test
+    @DisplayName("좋아요 실패(영양제 없음)")
+    void like_fail2(){
+        when(medicineRepository.findById(anyLong())).thenReturn(Optional.empty());
+        when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(false);
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> heartMedicineService.like(userId, medicineId));
+        assertEquals("해당하는 영양제가 없습니다.", exception.getMessage());
+    }
+    @Test
+    @DisplayName("좋아요 실패(이미 좋아요 누름)")
+    void like_fail3(){
+        when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(true);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> heartMedicineService.like(medicineId, userId));
+        assertEquals("좋아요를 이미 누름", exception.getMessage());
+    }
+    @Test
+    @DisplayName("좋아요 취소 성공")
+    void cancel_success(){
         Medicine medicine = Medicine.builder().id(medicineId).heartCount(100).build();
         when(medicineRepository.findById(medicineId)).thenReturn(Optional.of(medicine));
         when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(true);
@@ -68,6 +97,23 @@ class HeartMedicineServiceImplTest {
 
         assertEquals(1L, id);
         assertEquals(99, medicine.getHeartCount());
+    }
+    @Test
+    @DisplayName("좋아요 취소 실패(영양제가 없음)")
+    void cancel_fail1(){
+        when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(true);
+        when(medicineRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> heartMedicineService.cancel(medicineId, userId));
+        assertEquals("해당하는 영양제가 없습니다.", exception.getMessage());
+    }
+    @Test
+    @DisplayName("좋아요 취소 실패(좋아요를 누른 회원 아님)")
+    void cancel_fail2(){
+        when(heartMedicineRepository.existsByMedicineIdAndUserUserId(medicineId, userId)).thenReturn(false);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> heartMedicineService.cancel(medicineId, userId));
+        assertEquals("좋아요 누른 회원이 아님.", exception.getMessage());
     }
     @Test
     void findAll(){
@@ -109,6 +155,5 @@ class HeartMedicineServiceImplTest {
 
         assertTrue(checked1);
         assertFalse(checked2);
-
     }
 }
