fc973d0cbdd85aa5fa4bbc2e5640cd7a3a605322
(Jun) <(ddoly0106@gmail.com)>
2024-05-06 18:36:55 +0900

    feat : 자동완성 기능 수정 및 유저별 영양제 추천 기능 추가

--- src/main/java/com/example/demo/module/medicine/repository/QueryMedicineRepository.java
+++ src/main/java/com/example/demo/module/medicine/repository/QueryMedicineRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.module.medicine.repository;
 
+import com.example.demo.module.auto_complete.dto.AutoCompleteResult;
 import com.example.demo.module.medicine.dto.payload.MedicineSearchCond;
 import com.example.demo.module.medicine.dto.payload.MedicineOrderField;
 import com.example.demo.module.medicine.dto.payload.OrderSortCond;
@@ -7,6 +8,7 @@ import com.example.demo.module.medicine.entity.Medicine;
 import com.example.demo.module.medicine.entity.QMedicine;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -36,6 +38,7 @@ public class QueryMedicineRepository {
         this.em = em;
         this.query = new JPAQueryFactory(em);
     }
+
     public Page<Medicine> findAllBySearch(MedicineSearchCond medicineSearchCond, Pageable pageable) {
         OrderSortCond orderSortCond = medicineSearchCond.getOrderSortCond();
         List<Long> idList = query
@@ -77,15 +80,17 @@ public class QueryMedicineRepository {
         return PageableExecutionUtils.getPage(medicines, pageable, count::fetchOne);
     }
 
-    public List<String> getAutoComplete(String keyword, Integer limit) {
-        List<String> nameResult =
-                query.select(medicine.PRDLST_NM)
-                        .from(medicine)
-                        .orderBy(new OrderSpecifier<>(Order.ASC, medicine.PRDLST_NM))
-                        .where(nameLike(keyword))
-                        .offset(0)
-                        .limit(limit)
-                        .fetch();
+    public List<AutoCompleteResult> getAutoComplete(String keyword, Integer limit) {
+        List<AutoCompleteResult> result = query
+                .select(
+                        Projections.constructor(AutoCompleteResult.class,
+                                medicine.id, medicine.PRDLST_NM))
+                .from(medicine)
+                .where(nameLike(keyword))
+                .orderBy(new OrderSpecifier<>(Order.ASC, medicine.PRDLST_NM.length()))
+                .offset(0)
+                .limit(limit)
+                .fetch();
 
 //        QMedicine sub = new QMedicine("sub");
 //        List<String> companyResult =
@@ -98,9 +103,27 @@ public class QueryMedicineRepository {
 //                        .fetch();
 //        return Stream.concat(companyResult.stream(), nameResult.stream())
 //                .sorted(Comparator.comparing(String::length)).toList();
-        nameResult.sort(Comparator.comparing(String::length));
+        result.sort(Comparator.comparing(i -> i.getName().length()));
 
-        return nameResult;
+        return result;
+    }
+
+    public List<Medicine> findRecommend(List<Long> hashtagIds, Integer size) {
+        List<Long> idList = query
+                .select(medicine.id)
+                .from(medicine)
+                .join(medicine.hashtagList, medicineHashtag)
+                .where(hashtagIn(hashtagIds))
+                .groupBy(medicine.id)
+                .orderBy(new OrderSpecifier<>(Order.DESC, medicine.heartCount), new OrderSpecifier<>(Order.DESC, medicine.grade))
+                .limit(size)
+                .fetch();
+
+        return query
+                .select(medicine)
+                .from(medicine)
+                .where(medicine.id.in(idList))
+                .fetch();
     }
 
     private BooleanExpression heartCountGoe(Integer heartCount) {
@@ -136,6 +159,10 @@ public class QueryMedicineRepository {
         return (hashtagId == null || hashtagId == 0) ? null : medicineHashtag.hashtag.id.eq(hashtagId);
     }
 
+    private BooleanExpression hashtagIn(List<Long> hashtagIds) {
+        return (hashtagIds == null || hashtagIds.isEmpty()) ? null : medicineHashtag.hashtag.id.in(hashtagIds);
+    }
+
     private OrderSpecifier<?> setOrderBy(OrderSortCond orderSortCond) {
         if (orderSortCond == null) return new OrderSpecifier<>(Order.ASC, medicine.id);
 
@@ -150,4 +177,5 @@ public class QueryMedicineRepository {
             case ID -> new OrderSpecifier<>(sort, medicine.id);
         };
     }
+
 }
