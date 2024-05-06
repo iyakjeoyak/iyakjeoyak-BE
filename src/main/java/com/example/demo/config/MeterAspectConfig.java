0314c8c946301c1a84ddc22a2db02aede2c0ca97
(Jun) <(ddoly0106@gmail.com)>
2024-05-06 10:10:50 +0900

    feat : @Profile 적용

--- src/main/java/com/example/demo/config/MeterAspectConfig.java
+++ src/main/java/com/example/demo/config/MeterAspectConfig.java
@@ -5,8 +5,10 @@ import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("deploy")
 public class MeterAspectConfig {
 
 
