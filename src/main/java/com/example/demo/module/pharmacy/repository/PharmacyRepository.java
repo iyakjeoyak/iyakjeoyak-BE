a026ce11f1027cd7823878b7b42f90a90b1a147f
SeungjunNA <nsjjsng1@gmail.com>
2024-05-06 21:16:07 +0900

    feat: 약국 조회 쿼리 기능 최적화

--- src/main/java/com/example/demo/module/pharmacy/repository/PharmacyRepository.java
+++ src/main/java/com/example/demo/module/pharmacy/repository/PharmacyRepository.java
@@ -4,12 +4,15 @@ import com.example.demo.module.pharmacy.entity.Pharmacy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+    @Query("SELECT DISTINCT p FROM Pharmacy p LEFT JOIN FETCH p.businessHoursList bhl WHERE p.user.userId = ?1")
     Page<Pharmacy> findAllByUserUserId(Long userId, PageRequest pageRequest);
 
     List<Pharmacy> findAllByUserUserId(Long userId);
